95bc194033e7ecfbcb5b592a16a3e0968ae4d4bd
James Aubin <32369412+JOBBIN9422@users.noreply.github.com>
2019-09-24 12:36:36 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogReader
+{
+    public partial class Form1 : Form
+    {
+        private LogReader _reader;
+        private Thread _readLogThread;
+        private int _sleepIntervalMS;
+        public Form1()
+        {
+            InitializeComponent();
+            _reader = new LogReader();
+            _readLogThread = new Thread(ReadFromLogFile);
+
+            SleepIntervalUpDown.Value = 1;
+            _sleepIntervalMS = (int)(SleepIntervalUpDown.Value * 1000);
+        }
+
+        private void OpenLogFileButton_Click(object sender, EventArgs e)
+        {
+            if (OpenLogFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //open file and start reader thread
+                    _reader.LogFilePath = OpenLogFileDialog.FileName;
+                    WriteStatus($"Opened log file {_reader.LogFilePath}.");
+                    if (!_readLogThread.IsAlive)
+                    {
+                        _readLogThread.Start();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error opening file: {ex.ToString()}", "Error");
+                }
+            }
+        }
+
+        private void ReadFromLogFile()
+        {
+            while (true)
+            {
+                //need to run textbox updates on UI thread
+                this.Invoke((MethodInvoker)delegate()
+                {
+                    try
+                    {
+                        //write log contents to textbox and update status textbox
+                        LogFileTextBox.Text = _reader.ReadLogFile();
+                        LogFileTextBox.SelectionStart = LogFileTextBox.Text.Length;
+                        LogFileTextBox.ScrollToCaret();
+                        WriteStatus($"Read from log file (thread status: {_readLogThread.ThreadState}).");
+                    }
+                    catch (Exception ex)
+                    {
+                        WriteStatus($"EXCEPTION: { ex.Message} (thread status: { _readLogThread.ThreadState}).");
+                    }
+                });
+                Thread.Sleep(_sleepIntervalMS);
+            }
+        }
+
+        private void WriteStatus(string text)
+        {
+            StatusTextBox.AppendText($"[{DateTime.Now}]: {text}{Environment.NewLine}");
+        }
+
+        private void PauseCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            //kill the thread if it's alive
+            if (PauseCheckBox.Checked && _readLogThread.IsAlive)
+            {
+                _readLogThread.Abort();
+            }
+            else
+            {
+                //restart the thread
+                _readLogThread = new Thread(ReadFromLogFile);
+                _readLogThread.Start();
+            }
+        }
+
+        private void SleepIntervalUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            _sleepIntervalMS = (int)(SleepIntervalUpDown.Value * 1000);
+        }
+    }
+}
---
+++ LogReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace LogReader
+{
+    public class LogReader
+    {
+        public string LogFilePath { get; set; }
+
+        //public string ReadLogFile()
+        //{
+        //    return File.ReadAllText(LogFilePath);
+        //}
+
+        public string ReadLogFile()
+        {
+            using (FileStream fs = File.Open(LogFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (StreamReader reader = new StreamReader(fs))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
